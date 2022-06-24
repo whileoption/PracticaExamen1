d0fa24dcca558de8d51bd8527b4599c78c4f41da
KarinaMadrigal <101362675+KarinaMadrigal@users.noreply.github.com>
2022-06-23 18:50:45 -0600

    Start

---
+++ src/main/java/com/practicaExamen/PracticaExamenApplication.java
@@ -0,0 +1,13 @@
+package com.practicaExamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaExamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practicaExamen/PracticaExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicaExamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
